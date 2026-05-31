cf1e8a15bfbff81edcab10c344d1f203aaeecf1d
Nabin Shahi <er.nshahi@gmail.com>
2026-05-31 11:05:18 +0900

    Enhance Amazon todo list by adding a link to a CSV file for interview questions and ensuring existing resources are maintained.

---
+++ string/796-rotate-string.py
@@ -0,0 +1,35 @@
+"""
+796. Rotate String
+
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+Example 1:
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+Input: s = "abcde", goal = "abced"
+Output: false
+ 
+
+Constraints:
+1 <= s.length, goal.length <= 100
+s and goal consist of lowercase English letters.
+"""
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        n = len(s)
+        for i in range(n):
+            curr_string = []
+            for j in range(i, n + i):
+                idx = j % n
+                curr_string.append(s[idx])
+            if "".join(curr_string) == goal:
+                return True
+        return False
